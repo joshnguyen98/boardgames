8db4e14bd14d7ab0c179fd8f97bf87009f651041
joshnguyen98 <josh.nguyen@kpmg.co.uk>
2023-01-10 12:23:53 +0000

    6 and some css changes

--- src/components/SingleReview.jsx
+++ src/components/SingleReview.jsx
@@ -1,18 +1,24 @@
 import { useState, useEffect } from "react"
 import { useParams } from "react-router"
-import { getSingleReview } from "../api"
+import { getSingleReview, getComments} from "../api"
 
 const SingleReview = () => {
 
     const [isLoading, setIsLoading] = useState(true)
     const [review, setReview] = useState({})
+    const [comments, setComments] = useState([])
     const { review_id } = useParams()
     
     useEffect(() => {
+        setIsLoading(true)
         getSingleReview(review_id)
         .then((reviewFromApi) => {
             setReview(reviewFromApi)
             setIsLoading(false)
+        })
+        getComments(review_id)
+        .then((commentsFromApi) => {
+            setComments(commentsFromApi)
         })
     }, [])
 
@@ -20,15 +26,49 @@ const SingleReview = () => {
         return <p>Loading...</p>
     }
 
+    if (comments.length === 0) {
+        return (
+            <div>
+                <div className="singleReview">
+                    <h2>{review.title}</h2>
+                    <p>Votes: {review.votes}</p>
+                    <p>Comments: {review.comment_count}</p>
+                    <p>Designer: {review.designer}</p>
+                    <p>Author: {review.owner}</p>
+                    <img src={review.review_img_url} alt="Review Photo"></img>
+                    <p>{review.review_body}</p>
+                </div>
+                <div className="comment">
+                    <h3>No Comments.</h3>
+                </div>
+            </div>
+        )
+    }
+
     return (
-        <div className="singleReview">
-            <h2>{review.title}</h2>
-            <p>Votes: {review.votes}</p>
-            <p>Comments: {review.comment_count}</p>
-            <p>Designer: {review.designer}</p>
-            <p>Author: {review.owner}</p>
-            <img src={review.review_img_url} alt="Review Photo"></img>
-            <p>{review.review_body}</p>
+        <div>
+            <div className="singleReview">
+                <h2>{review.title}</h2>
+                <p>Votes: {review.votes}</p>
+                <p>Comments: {review.comment_count}</p>
+                <p>Designer: {review.designer}</p>
+                <p>Author: {review.owner}</p>
+                <img src={review.review_img_url} alt="Review Photo"></img>
+                <p>{review.review_body}</p>
+            </div>
+            <ol>
+                {comments.map((comment) => {
+                    return (
+                        <li className="comment" key={comment.comment_id}>
+                            <h3>
+                                By: {comment.author}
+                                <span> | </span>
+                                Votes: {comment.votes}
+                            </h3>
+                            <p>{comment.body}</p>
+                        </li>
+                )})}
+            </ol>
         </div>
     )
 }
